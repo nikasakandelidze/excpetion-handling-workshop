fc39ea8b1f42eeee6cfc35ddd1097eeb6dafa451
nikasakana <nikasakana@gmail.com>
2022-04-18 21:07:28 +0400

    added level 1 of examples of exceptions

---
+++ app/src/main/java/exceptionsWorkshop/level1/ExceptionalFlowExample.java
@@ -0,0 +1,29 @@
+package exceptionsWorkshop.level1;
+
+public class ExceptionalFlowExample {
+    /*
+        Output:
+            - Calling method 1
+            - Calling method 2
+            - Exception in thread "main" java.lang.RuntimeException: Something exceptional happened
+     */
+    public static void main(String[] args) {
+        method1();
+    }
+    private static void method1(){
+        System.out.println("Calling method 1");
+        method2();
+    }
+    private static void method2(){
+        System.out.println("Calling method 2");
+        method3();  // This method won't be called
+    }
+    private  static void method3(){
+        someExceptionalMethod();
+        System.out.println("Calling method 3");
+
+    }
+    private static void someExceptionalMethod(){
+        throw new RuntimeException("Something exceptional happened");
+    }
+}
---
+++ app/src/main/java/exceptionsWorkshop/level1/NormalFlowExample.java
@@ -0,0 +1,27 @@
+package exceptionsWorkshop.level1;
+
+public class NormalFlowExample {
+    /*
+        Output:
+        - Calling method 1
+        - Calling method 2
+        - Calling method 3
+     */
+    public static void main(String[] args) {
+        method1();
+    }
+
+    static void method1(){
+        System.out.println("Calling method 1");
+        method2();
+    }
+
+    static void method2(){
+        System.out.println("Calling method 2");
+        method3();
+    }
+
+    static void method3(){
+        System.out.println("Calling method 3");
+    }
+}
