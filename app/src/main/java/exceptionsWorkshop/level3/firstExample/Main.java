d08961380f1e45b5303d042b2f6aadf8c7eeb2f1
nikasakana <nikasakana@gmail.com>
2022-04-18 21:11:43 +0400

    removed redundant code

--- app/src/main/java/exceptionsWorkshop/level3/firstExample/Main.java
+++ app/src/main/java/exceptionsWorkshop/level3/firstExample/Main.java
@@ -1,12 +1,15 @@
 package exceptionsWorkshop.level3.firstExample;
 
 public class Main {
+
     public static void main(String[] args) {
-
-    }
-
-    public static class User{
-        public int id;
-        public String name;
+        var storage = new Storage();
+        var service = new Service(storage);
+        try{
+            var result = service.getAllUsernames();
+            System.out.println(result);
+        }catch (Service.OurServiceException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
